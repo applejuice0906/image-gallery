3ffc0f2caacba9a145f4b5cf465509c0cd8dd25b
applejuice0906 <30742023+applejuice0906@users.noreply.github.com>
2021-06-01 17:57:20 +0900

    update firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: 'AIzaSyD16qqfMg8C0u_sTXgjAT6v6-HRat4FnEQ',
-  authDomain: 'firegram-a2251.firebaseapp.com',
-  projectId: 'firegram-a2251',
-  storageBucket: 'firegram-a2251.appspot.com',
-  messagingSenderId: '289266577212',
-  appId: '1:289266577212:web:e8962aeb5e3fc8f410f839',
+  // Your firebase config...
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
